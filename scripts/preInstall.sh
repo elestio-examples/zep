9c4dce104cdca268bb5cd1d56849ac67ee647ecb
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-05-05 14:12:49 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -11,17 +11,17 @@ ZEP_AUTH_SECRET=${SECRET}
 EOT
 
 cat << EOT >> ./token.sh
+set -o allexport; source .env; set +o allexport;
 #!/bin/bash
 HEADER='{"alg": "HS256", "typ": "JWT"}'
 PAYLOAD='{"username": "admin"}'
-ENCODED_HEADER=$(echo -n "$HEADER" | base64 | tr -d '=' | tr '/+' '_-')
-ENCODED_PAYLOAD=$(echo -n "$PAYLOAD" | base64 | tr -d '=' | tr '/+' '_-')
-DATA="$ENCODED_HEADER.$ENCODED_PAYLOAD"
-SIGNATURE=$(echo -n "$DATA" | openssl dgst -binary -sha256 -hmac "$SECRET" | base64 | tr -d '=' | tr '/+' '_-')
-TOKEN="$DATA.$SIGNATURE"
+ENCODED_HEADER=\$(echo -n "\$HEADER" | base64 | tr -d '=' | tr '/+' '_-')
+ENCODED_PAYLOAD=\$(echo -n "\$PAYLOAD" | base64 | tr -d '=' | tr '/+' '_-')
+DATA="\$ENCODED_HEADER.\$ENCODED_PAYLOAD"
+SIGNATURE=\$(echo -n "\$DATA" | openssl dgst -binary -sha256 -hmac "\$ZEP_AUTH_SECRET" | base64 | tr -d '=' | tr '/+' '_-')
+TOKEN="\$DATA.\$SIGNATURE"
 
-echo "Generated JWT token: $TOKEN"
-
+echo "Generated JWT token: \$TOKEN"
 EOT
 
 chmod +x ./token.sh
